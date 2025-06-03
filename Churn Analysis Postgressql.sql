1eb03f9354e456343e7b99f12b87139863b22a7a
George-Njuguna <mailtomegeorge0@gmail.com>
2025-06-03 20:27:14 +0300

    sql analysis

--- Churn Analysis Postgressql.sql
+++ Churn Analysis Postgressql.sql
@@ -27,12 +27,34 @@ order by counts desc;
 
 
 /* getting the churn rate by gender */
-select gender , round((counts / sum(counts) over()) * 100 , 2 ) as rate 
-from (select gender ,
-             count(gender) as counts
-	  from churn
-	  group by gender) as t1
-order by rate desc;
+with t1(gender,count1)
+as (
+    select gender , count(gender)
+    from churn 
+    where churned = '1'
+    group by gender
+	),
+t2(gender,count0)
+as(
+   select gender , count(gender)
+   from churn 
+   where churned = '0'
+  group by gender
+  ),
+t3(gender,count1,count0)
+as(
+   select t1.gender , t1.count1 , t2.count0
+   from t1 
+   inner join t2 on t1.gender = t2.gender
+),
+t4(gender,count1,count0,total_counts)
+as(
+   select gender,count1,count0,(count1+count0) as total_counts
+   from t3
+)
+select gender,round((count1::decimal /total_counts)*100,1) as rate0 , round((count0::decimal /total_counts)*100,1) as rate1
+from t4;
+
 
 /* Getting number of bank clients in each geograpy */
 select geography , count(geography)  as counts
@@ -41,12 +63,34 @@ group by geography
 order by counts desc;
 
 /* getting the churn rate by geography */
-select geography , round((counts / sum(counts) over()) * 100 , 2 ) as rate 
-from (select geography ,
-             count(geography) as counts
-	  from churn
-	  group by geography) as t2
-order by rate desc;
+with t1(geography,count1)
+as (
+    select geography , count(geography)
+    from churn 
+    where churned = '1'
+    group by geography
+	),
+t2(geography,count0)
+as(
+   select geography , count(geography)
+   from churn 
+   where churned = '0'
+   group by geography
+  ),
+t3(geography,count1,count0)
+as(
+   select t1.geography , t1.count1 , t2.count0
+   from t1 
+   inner join t2 on t1.geography = t2.geography
+),
+t4(geography,count1,count0,total_counts)
+as(
+   select geography,count1,count0,(count1+count0) as total_counts
+   from t3
+)
+select geography,round((count1::decimal /total_counts)*100,1) as rate0 , round((count0::decimal /total_counts)*100,1) as rate1
+from t4;
+
 
 /* Getting number of bank clients by  membership */
 select isactivemember , count(isactivemember)  as counts
@@ -55,12 +99,34 @@ group by isactivemember
 order by counts desc;
 
 /* getting the churn rate by membership */
-select isactivemember , round((counts / sum(counts) over()) * 100 , 2 ) as rate 
-from (select isactivemember ,
-             count(isactivemember) as counts
-	  from churn
-	  group by isactivemember) as t3
-order by rate desc;
+with t1(isactivemember,count1)
+as (
+    select isactivemember , count(isactivemember)
+    from churn 
+    where churned = '1'
+    group by isactivemember
+	),
+t2(isactivemember,count0)
+as(
+   select isactivemember , count(isactivemember)
+   from churn 
+   where churned = '0'
+  group by isactivemember
+  ),
+t3(isactivemember,count1,count0)
+as(
+   select t1.isactivemember , t1.count1 , t2.count0
+   from t1 
+   inner join t2 on t1.isactivemember = t2.isactivemember
+),
+t4(isactivemember,count1,count0,total_counts)
+as(
+   select isactivemember,count1,count0,(count1+count0) as total_counts
+   from t3
+)
+select isactivemember,round((count1::decimal /total_counts)*100,1) as rate0 , round((count0::decimal /total_counts)*100,1) as rate1
+from t4;
+
 
 /* Getting number of bank clients by credit card ownership */
 select hascrcard , count(hascrcard)  as counts
@@ -68,10 +134,31 @@ from churn
 group by hascrcard
 order by counts desc;
 
-/* getting the churn rate by credit card ownership */
-select hascrcard , round((counts / sum(counts) over()) * 100 , 2 ) as rate 
-from (select hascrcard ,
-             count(hascrcard) as counts
-	  from churn
-	  group by hascrcard) as t4
-order by rate desc;
+/* getting attrition rate by credit card ownership*/
+with t1(hascrcard,count1)
+as (
+    select hascrcard , count(hascrcard)
+    from churn 
+    where churned = '1'
+    group by hascrcard
+	),
+t2(hascrcard,count0)
+as(
+   select hascrcard , count(hascrcard)
+   from churn 
+   where churned = '0'
+  group by hascrcard
+  ),
+t3(hascrcard,count1,count0)
+as(
+   select t1.hascrcard , t1.count1 , t2.count0
+   from t1 
+   inner join t2 on t1.hascrcard = t2.hascrcard
+),
+t4(hascrcard,count1,count0,total_counts)
+as(
+   select hascrcard,count1,count0,(count1+count0) as total_counts
+   from t3
+)
+select hascrcard,round((count1::decimal /total_counts)*100,1) as rate0 , round((count0::decimal /total_counts)*100,1) as rate1
+from t4;
