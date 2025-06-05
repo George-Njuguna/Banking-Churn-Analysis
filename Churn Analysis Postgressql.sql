e937c11648e89a2587d420171bfabf1c6d5f71a5
George-Njuguna <mailtomegeorge0@gmail.com>
2025-06-05 19:09:12 +0300

    .

--- Churn Analysis Postgressql.sql
+++ Churn Analysis Postgressql.sql
@@ -98,6 +98,13 @@ from churn
 group by isactivemember
 order by counts desc;
 
+/* Getting the active membership by gender*/
+
+select gender , isactivemember , count(isactivemember) as counts
+from churn
+group by gender , isactivemember;
+
+
 /* getting the churn rate by membership */
 with t1(isactivemember,count1)
 as (
@@ -227,7 +234,7 @@ with t1(churned,creditscore)
    select churned ,
     case 
       when creditscore  < 500 then '<500'
-      when creditscore  >= 500 and creditscore < 701 then '500-700'   /*grouping the ages*/    
+      when creditscore  >= 500 and creditscore < 701 then '500-700'   /*grouping the credit score*/    
       when creditscore  > 700 then '>700'
     end as creditscore
    from churn),
@@ -245,6 +252,64 @@ select
 from t2 a 
 join t2 b 
  on a.creditscore = b.creditscore
+where a.churned =  '1'
+ and b.churned = '0'
+order by rate1 desc;
+
+/*Getting the churn counts and rate by salary group*/
+with t1(churned,estimatedsalary)
+ as(
+   select churned ,
+    case 
+      when estimatedsalary  < 50000 then '<50000'
+      when estimatedsalary  >= 50000 and estimatedsalary < 100000 then '50000-100000'   /*grouping the Estimated salaries*/    
+      when estimatedsalary  >= 100000 and estimatedsalary < 150000 then '100000-150000'       
+      when estimatedsalary > 150000 then '>150000'
+    end as estimatedsalary
+   from churn),
+    t2(churned,estimatedsalary,counts)
+ as(
+   select churned , estimatedsalary , count(estimatedsalary)
+   from t1 
+   group by churned , estimatedsalary)
+select 
+       a.estimatedsalary as estimatedsalary,
+       a.counts as churn_yes_counts,
+       b.counts as churned_no_counts,
+       round((a.counts::decimal/(a.counts + b.counts))*100,1) as rate1,
+       round((b.counts::decimal/(a.counts + b.counts))*100,1) as rate0
+from t2 a 
+join t2 b 
+ on a.estimatedsalary = b.estimatedsalary
+where a.churned =  '1'
+ and b.churned = '0'
+order by rate1 desc;
+
+/*Getting the churn counts and rate by balance*/
+with t1(churned,balance)
+ as(
+   select churned ,
+    case 
+      when balance  < 1 then '0'
+      when balance  >= 1 and balance < 100000 then '1-10000'   /*grouping the balances*/    
+      when balance  >= 10000 and balance < 150000 then '10000-150000'      
+      when balance  > 150000 then '>150000'
+    end as balance
+   from churn),
+    t2(churned,balance,counts)
+ as(
+   select churned , balance , count(balance)
+   from t1 
+   group by churned , balance)
+select 
+       a.balance as balance,
+       a.counts as churn_yes_counts,
+       b.counts as churned_no_counts,
+       round((a.counts::decimal/(a.counts + b.counts))*100,1) as rate1,
+       round((b.counts::decimal/(a.counts + b.counts))*100,1) as rate0
+from t2 a 
+join t2 b 
+ on a.balance = b.balance
 where a.churned =  '1'
  and b.churned = '0'
 order by rate1 desc;
